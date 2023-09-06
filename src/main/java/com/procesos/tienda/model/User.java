eafb50ee0632fb517a434f9758864de432b693ba
LUZ <zaparanluz23.com>
2023-09-05 21:30:57 -0500

    crud para usuarios

--- src/main/java/com/procesos/tienda/model/User.java
+++ src/main/java/com/procesos/tienda/model/User.java
@@ -1,9 +1,15 @@
 package com.procesos.tienda.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity
 public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
@@ -12,6 +18,5 @@ public class User {
     private String phone;
     private String email;
     private String password;
-
-
 }
+
