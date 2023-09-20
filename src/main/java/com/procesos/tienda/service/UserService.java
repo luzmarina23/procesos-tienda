4b5efd2c54bc73d27205bddf997080951b97b020
luz <zaparanluz23@gmail.com>
2023-09-19 20:54:37 -0500

    Endpoints controlador y respuesta http

--- src/main/java/com/procesos/tienda/service/UserService.java
+++ src/main/java/com/procesos/tienda/service/UserService.java
@@ -5,6 +5,7 @@ import com.procesos.tienda.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -16,11 +17,12 @@ public class UserService {
         return userRepository.save(userReq);
     }
 
-    public Optional <User> getUserById(Long id){
-        return userRepository.findById(id);
+    public User getUserById(Long id){
+        return userRepository.findById(id).get();
     }
-    public User updateUser(User userReq){
-        Optional <User> userBd = userRepository.findById(userReq.getId());
+
+    public User updateUser(User userReq, Long id){
+        Optional <User> userBd = userRepository.findById(id);
         if(userBd.isEmpty()){
             return null;
         }
@@ -30,6 +32,7 @@ public class UserService {
         userBd.get().setPhone(userReq.getPhone());
         return userRepository.save(userBd.get());
     }
+
     public boolean deleteUser(Long id){
         Optional <User> userBd = userRepository.findById(id);
         if(userBd.isEmpty()){
@@ -37,5 +40,9 @@ public class UserService {
         }
         userRepository.delete(userBd.get());
         return true;
+    }
+
+    public List<User> findAllUsers(){
+        return (List<User>) userRepository.findAll();
     }
 }
